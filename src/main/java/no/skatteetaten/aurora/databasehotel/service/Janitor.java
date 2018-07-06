dcc3fa67ece86ddda8c7304df2ee100e79a06796
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-07-06 13:26:59 +0200

    Respecting the dropAllowed flag in the Janitor process

--- src/main/java/no/skatteetaten/aurora/databasehotel/service/Janitor.java
+++ src/main/java/no/skatteetaten/aurora/databasehotel/service/Janitor.java
@@ -2,6 +2,7 @@ package no.skatteetaten.aurora.databasehotel.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,16 +10,25 @@ import org.springframework.stereotype.Component;
 public class Janitor {
 
     private static Logger LOG = LoggerFactory.getLogger(Janitor.class);
+    private final boolean dropAllowed;
 
     private DatabaseHotelAdminService databaseHotelAdminService;
 
-    public Janitor(DatabaseHotelAdminService databaseHotelAdminService) {
+    public Janitor(
+        DatabaseHotelAdminService databaseHotelAdminService,
+        @Value("${databaseConfig.dropAllowed}") boolean dropAllowed
+    ) {
+
         this.databaseHotelAdminService = databaseHotelAdminService;
+        this.dropAllowed = dropAllowed;
     }
 
     @Scheduled(fixedDelay = 3600 * 1000, initialDelay = 60 * 1000)
     public void deleteOldUnusedSchemas() {
 
+        if (!dropAllowed) {
+            return;
+        }
         LOG.info("Periodic deletion of old unused schemas");
         databaseHotelAdminService.findAllDatabaseInstances().forEach(DatabaseInstance::deleteUnusedSchemas);
     }
