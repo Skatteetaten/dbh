f4c49efdeed58121781df9d628e7bd7c232da36a
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2019-05-14 10:56:37 +0200

    ktlint

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/DbhInitializer.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/DbhInitializer.kt
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import java.lang.String.format
 
 @Component
 class DbhInitializer(
