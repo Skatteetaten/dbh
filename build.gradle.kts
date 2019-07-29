60ec44cf061c19b6b71690be49a6ce5312e80471
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-07-29 14:39:01 +0200

    updated testlogger theme

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.40"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.40"
@@ -50,6 +52,10 @@ dependencies {
         // Required to compile test code
         testImplementation(devtools)
     }
+}
+
+testlogger {
+    theme = ThemeType.PLAIN
 }
 
 tasks {
