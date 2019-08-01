9fa6c271bd91ad4df925b033cc631a563a917ade
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2019-08-01 15:28:03 +0200

    Reapplying some refactorings

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/service/ExternalSchemaManager.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/service/ExternalSchemaManager.kt
@@ -1,5 +1,6 @@
 package no.skatteetaten.aurora.databasehotel.service
 
+import com.google.common.collect.Lists
 import no.skatteetaten.aurora.databasehotel.DatabaseEngine.ORACLE
 import no.skatteetaten.aurora.databasehotel.dao.DatabaseHotelDataDao
 import no.skatteetaten.aurora.databasehotel.dao.Schema
@@ -14,52 +15,50 @@ import no.skatteetaten.aurora.databasehotel.service.DatabaseInstance.UserType.SC
 import no.skatteetaten.aurora.databasehotel.service.internal.DatabaseSchemaBuilder
 import java.math.BigDecimal
 import java.util.HashMap
-import java.util.Optional
 
 class ExternalSchemaManager(private val databaseHotelDataDao: DatabaseHotelDataDao) {
 
-    fun findSchemaById(schemaId: String): DatabaseSchema? {
+    fun findSchemaById(schemaId: String): DatabaseSchema? =
+        databaseHotelDataDao.findSchemaDataById(schemaId)?.let(this::getDatabaseSchemaFromSchemaData)
 
-        return databaseHotelDataDao.findSchemaDataById(schemaId)
-            ?.let(this::getDatabaseSchemaFromSchemaData)
-    }
-
-    fun findAllSchemas(): Set<DatabaseSchema> {
-
-        // TODO: Iterating like this may (will) become a performance bottleneck at some point.
-        return databaseHotelDataDao.findAllSchemaDataBySchemaType(SCHEMA_TYPE_EXTERNAL)
+    fun findAllSchemas(): Set<DatabaseSchema> =
+        databaseHotelDataDao.findAllSchemaDataBySchemaType(SCHEMA_TYPE_EXTERNAL)
+            // TODO: Iterating like this may (will) become a performance bottleneck at some point.
             .map(this::getDatabaseSchemaFromSchemaData).toSet()
-    }
 
     fun registerSchema(
         username: String,
         password: String,
         jdbcUrl: String,
         labelMap: Map<String, String?>
-    ): DatabaseSchema = databaseHotelDataDao.run {
-        val schemaData = createSchemaData(username, SCHEMA_TYPE_EXTERNAL)
-        val externalSchema = registerExternalSchema(schemaData.id, jdbcUrl)
-        replaceLabels(schemaData.id, labelMap)
-        val user = createUser(schemaData.id, SCHEMA.toString(), username, password)
-        createDatabaseSchema(schemaData, externalSchema, listOf(user))
+    ): DatabaseSchema {
+
+        val schemaData = databaseHotelDataDao.createSchemaData(username, SCHEMA_TYPE_EXTERNAL)
+        val externalSchema = databaseHotelDataDao.registerExternalSchema(schemaData.id, jdbcUrl)
+        databaseHotelDataDao.replaceLabels(schemaData.id, labelMap)
+        val user = databaseHotelDataDao.createUser(schemaData.id, SCHEMA.toString(), username, password)
+        val users = Lists.newArrayList(user)
+
+        return createDatabaseSchema(schemaData, externalSchema, users)
     }
 
-    fun deleteSchema(schemaId: String) = databaseHotelDataDao.run {
-        deleteSchemaData(schemaId)
-        deleteUsersForSchema(schemaId)
-        deleteLabelsForSchema(schemaId)
-        deleteExternalSchema(schemaId)
+    fun deleteSchema(schemaId: String) {
+
+        databaseHotelDataDao.deleteSchemaData(schemaId)
+        databaseHotelDataDao.deleteUsersForSchema(schemaId)
+        databaseHotelDataDao.deleteLabelsForSchema(schemaId)
+        databaseHotelDataDao.deleteExternalSchema(schemaId)
     }
 
-    fun replaceLabels(schema: DatabaseSchema, labels: Map<String, String?>) {
-
-        schema.labels = labels
-        databaseHotelDataDao.replaceLabels(schema.id, labels)
-    }
-
-    fun updateConnectionInfo(schemaId: String, username: String?, jdbcUrl: String?, password: String?) {
-
-        databaseHotelDataDao.updateExternalSchema(schemaId, username, jdbcUrl, password)
+    fun updateSchema(
+        schema: DatabaseSchema,
+        labels: Map<String, String?>,
+        username: String?,
+        password: String?
+    ): DatabaseSchema {
+        replaceLabels(schema, labels)
+        updateConnectionInfo(schema.id, username, schema.jdbcUrl, password)
+        return findSchemaById(schema.id) ?: throw IllegalStateException("Unable to find schema ${schema.id}")
     }
 
     private fun createDatabaseSchema(
@@ -69,16 +68,32 @@ class ExternalSchemaManager(private val databaseHotelDataDao: DatabaseHotelDataD
     ): DatabaseSchema {
 
         val schema = Schema(schemaData.name, externalSchema.createdDate!!)
-        val labels = databaseHotelDataDao.findAllLabelsForSchema(schemaData.id)
-        val metaInfo = DatabaseInstanceMetaInfo(ORACLE, "external", "-", 0, false, HashMap())
 
-        return DatabaseSchemaBuilder(metaInfo, externalSchema::jdbcUrl as JdbcUrlBuilder)
-            .createOne(
-                schemaData, schema, users,
-                Optional.ofNullable(labels),
-                SchemaSize(schemaData.name, BigDecimal.ZERO),
-                EXTERNAL
-            )
+        val labels = databaseHotelDataDao.findAllLabelsForSchema(schemaData.id)
+
+        val metaInfo = DatabaseInstanceMetaInfo(ORACLE, "external", "-", 0, false, HashMap())
+        val jdbcUrlBuilder = object : JdbcUrlBuilder {
+            override fun create(dbHost: String, port: Int, database: String?): String {
+                return externalSchema.jdbcUrl ?: ""
+            }
+        }
+
+        return DatabaseSchemaBuilder(metaInfo, jdbcUrlBuilder).createOne(
+            schemaData, schema, users, labels,
+            SchemaSize(schemaData.name, BigDecimal.ZERO),
+            EXTERNAL
+        )
+    }
+
+    private fun replaceLabels(schema: DatabaseSchema, labels: Map<String, String?>) {
+
+        schema.labels = labels
+        databaseHotelDataDao.replaceLabels(schema.id, labels)
+    }
+
+    private fun updateConnectionInfo(schemaId: String, username: String?, jdbcUrl: String?, password: String?) {
+
+        databaseHotelDataDao.updateExternalSchema(schemaId, username, jdbcUrl, password)
     }
 
     private fun getDatabaseSchemaFromSchemaData(schemaData: SchemaData): DatabaseSchema {
