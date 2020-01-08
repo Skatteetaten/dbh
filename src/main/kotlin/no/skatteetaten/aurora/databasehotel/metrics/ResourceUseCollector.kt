6ee037d044ad8754838e48ea2a1da57f430068fd
Bent André Solheim <BentAndre.Solheim@Skatteetaten.no>
2020-01-08 13:07:55 +0100

    Added more dimensions to schema size metric

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/metrics/ResourceUseCollector.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/metrics/ResourceUseCollector.kt
@@ -1,151 +1,124 @@
-package no.skatteetaten.aurora.databasehotel.metrics;
+package no.skatteetaten.aurora.databasehotel.metrics
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import com.google.common.collect.Sets;
-
-import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.MeterRegistry;
-import no.skatteetaten.aurora.databasehotel.domain.DatabaseSchema;
-import no.skatteetaten.aurora.databasehotel.service.DatabaseHotelService;
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
+import no.skatteetaten.aurora.databasehotel.domain.DatabaseSchema
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.AFFILIATION
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.APPLICATION
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.DATABASE_ENGINE
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.DATABASE_HOST
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.ENVIRONMENT
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.NAMESPACE
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.SCHEMA_ID
+import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.SCHEMA_TYPE
+import no.skatteetaten.aurora.databasehotel.service.DatabaseHotelService
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.HashMap
 
 @Component
-public class ResourceUseCollector {
+class ResourceUseCollector(
+    private val databaseHotelService: DatabaseHotelService,
+    private val registry: MeterRegistry
+) {
+    private val schemaSizeValues = mutableMapOf<String, SchemaSizeValue>()
 
-    private static final Logger LOG = LoggerFactory.getLogger(ResourceUseCollector.class);
-    private static final String METRIC_NAME = "aurora.dbh.schema.size.mb";
-    private static final String ENVIRONMENT_LABEL = "environment";
-    private static final String APP_LABEL = "application";
-    private static final String AFFILIATION_LABEL = "affiliation";
-    private static final String ENVIRONMENT_METRIC_LABEL = "namespace";
-    private final MeterRegistry registry;
-    private final DatabaseHotelService databaseHotelService;
-    private final Map<String, SchemaSizeValue> schemaSizeValues = new HashMap<>();
+    private val gauges = mutableMapOf<String, Gauge>()
 
-    public ResourceUseCollector(DatabaseHotelService databaseHotelService, MeterRegistry registry) {
-        this.registry = registry;
-        this.databaseHotelService = databaseHotelService;
-    }
-
-    @Scheduled(fixedDelayString = "${metrics.resourceUseCollectInterval}", initialDelay = 15000)
-    public void collectResourceUseMetrics() {
-
-        long start = System.currentTimeMillis();
-        LOG.info("Collecting resource use metrics");
-
-        Set<DatabaseSchema> allSchemas = databaseHotelService.findAllDatabaseSchemas(null, new HashMap<>(), false);
-        LOG.debug("Found {} schemas total", allSchemas.size());
-
-        List<DatabaseSchema> databaseSchemas = allSchemas.stream()
-            .filter(s -> containsEveryKey(s.getLabels(), ENVIRONMENT_LABEL, APP_LABEL, AFFILIATION_LABEL))
-            .collect(Collectors.toList());
+    @Scheduled(fixedDelayString = "\${metrics.resourceUseCollectInterval}", initialDelay = 5000)
+    fun collectResourceUseMetrics() {
+        val start = System.currentTimeMillis()
+        LOG.info("Collecting resource use metrics")
+        val allSchemas = databaseHotelService.findAllDatabaseSchemas(null, HashMap(), true)
+        LOG.debug("Found {} schemas total", allSchemas.size)
+        val databaseSchemas = allSchemas
+            .filter { it.labels.containsEveryKey(NAMESPACE_LABEL, APP_LABEL, AFFILIATION_LABEL) }
+            .filter { it.active }
 
         // Let's update the SchemaSizeValues that backs the schema size gauges
-        databaseSchemas.forEach(schema -> {
-            SchemaSizeValue existingValue = schemaSizeValues.get(schema.getId());
+        databaseSchemas.forEach { schema ->
+            val existingValue = schemaSizeValues[schema.id]
             if (existingValue != null) {
-                existingValue.setSizeMb(schema.getSizeMb());
+                existingValue.sizeBytes = schema.sizeMb
             } else {
-                SchemaSizeValue value = new SchemaSizeValue(schema.getId(), schema.getSizeMb());
-                schemaSizeValues.put(schema.getId(), value);
-                registerGaugeForSchema(schema, value);
+                val value = SchemaSizeValue(schema.id, schema.sizeMb)
+                schemaSizeValues[schema.id] = value
+                registerGaugeForSchema(schema, value)
             }
-        });
+        }
 
         // Remove the SchemaSizeValues that does not currently have a DatabaseSchema object. This indicates that
         // the DatabaseSchema has been deleted since the last time we updated the gauges. Deleting the value that
         // backs a Gauge *should* also in fact delete the Gauge since the reference to the SchemaSizeValue is a
-        // WeakReference. This does not appear to happen (bug?(!)), and to make matters worse, deleting the Gauge
-        // from the MeterRegistry does not delete it from the io.prometheus.client.CollectorRegistry instance that is
-        // used to render the data for Prometheus. I'm hoping and assuming that these issues will be fixed in a future
-        // version of micrometer.io, so I'm leaving the implementation as is - even if that means leaving deleted
-        // DatabaseSchemas with a NaN size.
-        schemaSizeValues.keySet().removeIf(
-            schemaId -> {
-                boolean isAnExistingSchema =
-                    databaseSchemas.stream().anyMatch(databaseSchema -> schemaId.equals(databaseSchema.getId()));
-                boolean isSchemaRemovedSinceLastIteration = !isAnExistingSchema;
-                return isSchemaRemovedSinceLastIteration;
-            });
-
-        LOG.info("Resource use metrics collected for {} schemas in {} millis", databaseSchemas.size(),
-            System.currentTimeMillis() - start);
+        // WeakReference. This does not appear to happen (bug?(!)), so we have to remove the gauge from the registry
+        // manually.
+        schemaSizeValues.keys.removeIf { schemaId: String ->
+            val isAnExistingSchema = databaseSchemas.any { schemaId == it.id }
+            val isSchemaRemovedSinceLastIteration = !isAnExistingSchema
+            if (isSchemaRemovedSinceLastIteration) registry.remove(gauges[schemaId]!!)
+            isSchemaRemovedSinceLastIteration
+        }
+        LOG.info(
+            "Resource use metrics collected for {} schemas in {} millis", databaseSchemas.size,
+            System.currentTimeMillis() - start
+        )
     }
 
-    @SafeVarargs
-    private static <K> boolean containsEveryKey(Map<K, ?> map, K... keys) {
-
-        Set<K> keySet = Sets.newHashSet(keys);
-        return map.keySet().containsAll(keySet);
+    private fun registerGaugeForSchema(schema: DatabaseSchema, value: SchemaSizeValue) {
+        val gauge = Gauge.builder(METRIC_NAME, value, { it.sizeBytes * 1024 * 1024 })
+            .baseUnit("bytes")
+            .description("the size of the schema")
+            .tags(createLabelsArray(schema))
+            .register(registry)
+        gauges[schema.id] = gauge
     }
 
-    private void registerGaugeForSchema(DatabaseSchema schema, SchemaSizeValue value) {
-
-        Map<String, String> labels = schema.getLabels();
-        Gauge.builder(METRIC_NAME, value, SchemaSizeValue::getSizeMb)
-            .description("the size of the individual registered schemas")
-            .tags(createLabelsArray(labels))
-            .register(registry);
+    private fun createLabelsArray(schema: DatabaseSchema): List<Tag> {
+        val labels = schema.labels
+        val t = { tag: Tags, value: String? -> Tag.of(tag.name2, value ?: "UNKNOWN") }
+        val namespace = labels[NAMESPACE_LABEL]?.removeSuffix("-")
+        val affiliation = labels[AFFILIATION_LABEL]
+        val environment = affiliation?.let { namespace?.getEnvironment(it) }
+        return listOf(
+            t(SCHEMA_ID, schema.id),
+            t(AFFILIATION, affiliation),
+            t(APPLICATION, labels[APP_LABEL]),
+            t(ENVIRONMENT, environment),
+            t(NAMESPACE, namespace),
+            t(DATABASE_HOST, schema.databaseInstanceMetaInfo.host),
+            t(DATABASE_ENGINE, schema.databaseInstanceMetaInfo.engine.name),
+            t(SCHEMA_TYPE, schema.type.name)
+        )
     }
 
-    private String[] createLabelsArray(Map<String, String> labels) {
-        return new String[] { AFFILIATION_LABEL, labels.get(AFFILIATION_LABEL),
-            APP_LABEL, labels.get(APP_LABEL),
-            ENVIRONMENT_METRIC_LABEL, labels.get(ENVIRONMENT_LABEL) };
+    data class SchemaSizeValue(var id: String, var sizeBytes: Double)
+
+    enum class Tags(val name2: String) {
+        SCHEMA_ID("id"),
+        SCHEMA_TYPE("schemaType"),
+        DATABASE_HOST("databaseHost"),
+        DATABASE_ENGINE("databaseEngine"),
+        AFFILIATION("affiliation"),
+        ENVIRONMENT("environment"),
+        APPLICATION("application"),
+        NAMESPACE("namespace")
     }
 
-    public static class SchemaSizeValue {
-        private String id;
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ResourceUseCollector::class.java)
+        private const val METRIC_NAME = "aurora.dbh.schema.size.bytes"
+        private const val NAMESPACE_LABEL = "environment"
+        private const val APP_LABEL = "application"
+        private const val AFFILIATION_LABEL = "affiliation"
+    }
 
-        private double sizeMb;
-
-        public SchemaSizeValue(String id, double sizeMb) {
-            this.id = id;
-            this.sizeMb = sizeMb;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public double getSizeMb() {
-            return sizeMb;
-        }
-
-        public void setSizeMb(double sizeMb) {
-            this.sizeMb = sizeMb;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
-            SchemaSizeValue that = (SchemaSizeValue) o;
-
-            return id.equals(that.id);
-        }
-
-        @Override
-        public int hashCode() {
-            return id.hashCode();
-        }
+    private fun String.getEnvironment(affiliation: String): String {
+        val environment = this.replace("^$affiliation".toRegex(), "").removePrefix("-")
+        return if (environment.isBlank()) affiliation else environment
     }
 }
+
+private fun <K> Map<K, *>.containsEveryKey(vararg keys: K) = this.keys.containsAll(setOf(*keys))
+
