78ff8f612dda674d6093e9adc240615769e5bb23
Bent André Solheim <BentAndre.Solheim@Skatteetaten.no>
2020-01-20 14:23:07 +0100

    Feature/aos 3964 metrikker (#42)
    * working with duplicated code
    
    * wip
    
    * added aurora_dbh_schema_count with cleanup
    
    * Clean up
    
    * Ktlint
    
    Co-authored-by: Kim Lindtner <kim.lindtner@skatteetaten.no>

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/metrics/ResourceUseCollector.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/metrics/ResourceUseCollector.kt
@@ -5,14 +5,14 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
 import java.util.HashMap
 import no.skatteetaten.aurora.databasehotel.domain.DatabaseSchema
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.AFFILIATION
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.APPLICATION
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.DATABASE_ENGINE
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.DATABASE_HOST
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.ENVIRONMENT
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.NAMESPACE
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.SCHEMA_ID
-import no.skatteetaten.aurora.databasehotel.metrics.ResourceUseCollector.Tags.SCHEMA_TYPE
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.AFFILIATION
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.APPLICATION
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.DATABASE_ENGINE
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.DATABASE_HOST
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.ENVIRONMENT
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.NAMESPACE
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.SCHEMA_ID
+import no.skatteetaten.aurora.databasehotel.metrics.MetricTag.SCHEMA_TYPE
 import no.skatteetaten.aurora.databasehotel.service.DatabaseHotelService
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
@@ -23,15 +23,18 @@ class ResourceUseCollector(
     private val databaseHotelService: DatabaseHotelService,
     private val registry: MeterRegistry
 ) {
-    private val schemaGauges = mutableMapOf<String, Pair<SchemaMetricData, Gauge>>()
+    private val schemaGauges = mutableMapOf<String, Pair<GaugeValue, Gauge>>()
+    private val schemaCountGauges = mutableMapOf<String, Pair<GaugeValue, Gauge>>()
 
     @Scheduled(fixedDelayString = "\${metrics.resourceUseCollectInterval}", initialDelay = 5000)
     fun collectResourceUseMetrics() {
+
+        LOG.info("Collecting resource use metrics")
         val start = System.currentTimeMillis()
 
         val databaseSchemas = loadDatabaseSchemas()
-        registerOrUpdateMetrics(databaseSchemas)
-        removeMetricsForDeletedSchemas(databaseSchemas)
+        handleSchemaSizeMetrics(databaseSchemas)
+        handleSchemaCountMetrics(databaseSchemas)
 
         LOG.info(
             "Resource use metrics collected for {} schemas in {} millis", databaseSchemas.size,
@@ -39,98 +42,136 @@ class ResourceUseCollector(
         )
     }
 
-    private fun loadDatabaseSchemas(): List<DatabaseSchema> {
-        LOG.info("Collecting resource use metrics")
-        val databaseSchemas = databaseHotelService.findAllDatabaseSchemas(null, HashMap(), true)
-            .filter { it.labels.containsEveryKey(NAMESPACE_LABEL, APP_LABEL, AFFILIATION_LABEL) }
-        LOG.debug("Found {} schemas total", databaseSchemas.size)
-        return databaseSchemas
-    }
+    private fun loadDatabaseSchemas() =
+        databaseHotelService.findAllDatabaseSchemas(null, HashMap(), true)
+            .toList()
+            .also { LOG.debug("Found {} schemas total", it.size) }
 
-    private fun registerOrUpdateMetrics(databaseSchemas: List<DatabaseSchema>) {
-        databaseSchemas.forEach { schema ->
-            val existing = schemaGauges[schema.id]
+    private fun handleSchemaSizeMetrics(databaseSchemas: List<DatabaseSchema>) {
+        val currentMetricIds = databaseSchemas.map { schema ->
+            val id = schema.id
+            val existing = schemaGauges[id]
             if (existing != null) {
                 val metricData = existing.first
-                metricData.sizeBytes = schema.sizeMb * 1024 * 1024
+                metricData.value = schema.sizeMb * 1024 * 1024
             } else {
-                val data = SchemaMetricData(schema.id, schema.sizeMb)
+                val data = GaugeValue(id, schema.sizeMb)
                 val gauge = registerSchemaSizeGauge(schema, data)
-                schemaGauges[schema.id] = data to gauge
+                schemaGauges[id] = data to gauge
             }
+            id
         }
+
+        schemaGauges.removeDeprecatedMetrics(currentMetricIds)
     }
 
-    /**
-     * Remove the SchemaSizeValues that does not currently have a DatabaseSchema object. This indicates that
-     * the DatabaseSchema has been deleted since the last time we updated the gauges. Deleting the value that
-     * backs a Gauge *should* also in fact delete the Gauge since the reference to the SchemaSizeValue is a
-     * WeakReference. This does not appear to happen (bug?(!)), so we have to remove the gauge from the registry
-     * manually.
-     */
-    private fun removeMetricsForDeletedSchemas(databaseSchemas: List<DatabaseSchema>) {
-        val schemasToRemove = schemaGauges.keys
-            .filter { schemaId: String -> !databaseSchemas.any { schemaId == it.id } }
+    private fun handleSchemaCountMetrics(databaseSchemas: List<DatabaseSchema>) {
+        val grouped: Map<CountGroup, List<DatabaseSchema>> = groupDatabaseSchemasForCounting(databaseSchemas)
+        val currentMetricIds: List<String> = grouped.map { (groupKeys, schemas) ->
+            val id = groupKeys.id
+            val existing = schemaCountGauges[id]
+            if (existing != null) {
+                val metricData = existing.first
+                metricData.value = schemas.size.toDouble()
+            } else {
+                val data = GaugeValue(id, schemas.size.toDouble())
+                val gauge = registerSchemaCountGauge(groupKeys, data)
+                schemaCountGauges[id] = data to gauge
+            }
+            id
+        }
 
-        schemasToRemove
-            .mapNotNull { schemaGauges[it] }
+        schemaCountGauges.removeDeprecatedMetrics(currentMetricIds)
+    }
+
+    private fun groupDatabaseSchemasForCounting(databaseSchemas: List<DatabaseSchema>) =
+        databaseSchemas.groupBy {
+            mapOf(
+                "state" to if (it.active) "ACTIVE" else "COOLDOWN",
+                "databaseHost" to it.databaseInstanceMetaInfo.host,
+                "databaseEngine" to it.databaseInstanceMetaInfo.engine.name,
+                "schemaType" to it.type.name,
+                "affiliation" to it.labels[AFFILIATION_LABEL]
+            )
+        }
+
+    private fun registerSchemaSizeGauge(schema: DatabaseSchema, value: GaugeValue): Gauge {
+        return Gauge.builder(SCHEMA_SIZE_METRIC_NAME, value, { it.value })
+            .baseUnit("bytes")
+            .description("the size of the schema")
+            .tags(schema.metricTags)
+            .register(registry)
+    }
+
+    private fun registerSchemaCountGauge(groupKeys: CountGroup, value: GaugeValue): Gauge {
+        val tags = groupKeys.map { (k, v) -> tagOfValueOrUnknown(k, v) }
+        return Gauge.builder(SCHEMA_COUNT_METRIC_NAME, value, { it.value })
+            .baseUnit("count")
+            .description("The amount of schemas")
+            .tags(tags)
+            .register(registry)
+    }
+
+    private fun MutableMap<String, Pair<GaugeValue, Gauge>>.removeDeprecatedMetrics(currentMetricIds: List<String>) {
+        val metricsToRemove = keys.minus(currentMetricIds)
+        metricsToRemove
+            .mapNotNull { this[it] }
             .forEach { (data, gauge) ->
-                schemaGauges.remove(data.id)
+                remove(data.id)
                 registry.remove(gauge.id)
             }
     }
 
-    private fun registerSchemaSizeGauge(schema: DatabaseSchema, value: SchemaMetricData): Gauge {
-        return Gauge.builder(METRIC_NAME, value, { it.sizeBytes })
-            .baseUnit("bytes")
-            .description("the size of the schema")
-            .tags(createLabelsArray(schema))
-            .register(registry)
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ResourceUseCollector::class.java)
+        private const val SCHEMA_SIZE_METRIC_NAME = "aurora.dbh.schema.size.bytes"
+        private const val SCHEMA_COUNT_METRIC_NAME = "aurora.dbh.schema.count"
     }
+}
 
-    private fun createLabelsArray(schema: DatabaseSchema): List<Tag> {
-        val labels = schema.labels
-        val t = { tag: Tags, value: String? -> Tag.of(tag.tagName, value ?: "UNKNOWN") }
+private enum class MetricTag(val tagName: String) {
+    SCHEMA_ID("id"),
+    SCHEMA_TYPE("schemaType"),
+    DATABASE_HOST("databaseHost"),
+    DATABASE_ENGINE("databaseEngine"),
+    AFFILIATION("affiliation"),
+    ENVIRONMENT("environment"),
+    APPLICATION("application"),
+    NAMESPACE("namespace")
+}
+
+private val NAMESPACE_LABEL = ENVIRONMENT.tagName
+private val APP_LABEL = APPLICATION.tagName
+private val AFFILIATION_LABEL = AFFILIATION.tagName
+
+private val DatabaseSchema.metricTags
+    get(): List<Tag> {
+        val labels = this.labels
+        val t = { tag: MetricTag, value: String? -> tagOfValueOrUnknown(tag.tagName, value) }
         val namespace = labels[NAMESPACE_LABEL]?.removeSuffix("-")
         val affiliation = labels[AFFILIATION_LABEL]
         val environment = affiliation?.let { namespace?.getEnvironment(it) }
         return listOf(
-            t(SCHEMA_ID, schema.id),
+            t(SCHEMA_ID, this.id),
             t(AFFILIATION, affiliation),
             t(APPLICATION, labels[APP_LABEL]),
             t(ENVIRONMENT, environment),
             t(NAMESPACE, namespace),
-            t(DATABASE_HOST, schema.databaseInstanceMetaInfo.host),
-            t(DATABASE_ENGINE, schema.databaseInstanceMetaInfo.engine.name),
-            t(SCHEMA_TYPE, schema.type.name)
+            t(DATABASE_HOST, this.databaseInstanceMetaInfo.host),
+            t(DATABASE_ENGINE, this.databaseInstanceMetaInfo.engine.name),
+            t(SCHEMA_TYPE, this.type.name)
         )
     }
 
-    data class SchemaMetricData(var id: String, var sizeBytes: Double)
+data class GaugeValue(var id: String, var value: Double)
 
-    enum class Tags(val tagName: String) {
-        SCHEMA_ID("id"),
-        SCHEMA_TYPE("schemaType"),
-        DATABASE_HOST("databaseHost"),
-        DATABASE_ENGINE("databaseEngine"),
-        AFFILIATION("affiliation"),
-        ENVIRONMENT("environment"),
-        APPLICATION("application"),
-        NAMESPACE("namespace")
-    }
+private fun tagOfValueOrUnknown(name: String, value: String?) = Tag.of(name, value ?: "UNKNOWN")
 
-    companion object {
-        private val LOG = LoggerFactory.getLogger(ResourceUseCollector::class.java)
-        private const val METRIC_NAME = "aurora.dbh.schema.size.bytes"
-        private const val NAMESPACE_LABEL = "environment"
-        private const val APP_LABEL = "application"
-        private const val AFFILIATION_LABEL = "affiliation"
-    }
+private typealias CountGroup = Map<String, String?>
 
-    private fun String.getEnvironment(affiliation: String): String {
-        val environment = this.replace("^$affiliation".toRegex(), "").removePrefix("-")
-        return if (environment.isBlank()) affiliation else environment
-    }
+private val CountGroup.id get() = this.toSortedMap().map { (k, v) -> "$k=$v" }.joinToString(",")
+
+private fun String.getEnvironment(affiliation: String): String {
+    val environment = this.replace("^$affiliation".toRegex(), "").removePrefix("-")
+    return if (environment.isBlank()) affiliation else environment
 }
-
-private fun <K> Map<K, *>.containsEveryKey(vararg keys: K) = this.keys.containsAll(setOf(*keys))
