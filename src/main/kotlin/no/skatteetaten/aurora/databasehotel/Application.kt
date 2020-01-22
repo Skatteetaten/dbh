bbc847efae7cd4c9d4ce95868c857af482c6c5f7
Bent André Solheim <BentAndre.Solheim@Skatteetaten.no>
2020-01-22 16:01:16 +0100

    .

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/Application.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/Application.kt
@@ -8,13 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 
 @SpringBootApplication(exclude = [
-    org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration::class,
-    org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration::class,
-    org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration::class,
-    org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration::class,
-    org.springframework.boot.actuate.autoconfigure.metrics.cache.CacheMetricsAutoConfiguration::class,
-    org.springframework.boot.actuate.autoconfigure.metrics.web.client.HttpClientMetricsAutoConfiguration::class,
-    org.springframework.boot.actuate.autoconfigure.metrics.web.servlet.WebMvcMetricsAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.cache.CacheMetricsAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.web.client.HttpClientMetricsAutoConfiguration::class,
+//    org.springframework.boot.actuate.autoconfigure.metrics.web.servlet.WebMvcMetricsAutoConfiguration::class,
     DataSourceAutoConfiguration::class
 ])
 class Application
