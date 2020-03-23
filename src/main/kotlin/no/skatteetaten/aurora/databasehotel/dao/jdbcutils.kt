f4ec4e802a7743f4633c79bea669f3706ebd186b
Kim Lindtner <Kim.Lindtner@skatteetaten.no>
2020-03-23 15:31:01 +0100

    cleanup

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/dao/jdbcutils.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/dao/jdbcutils.kt
@@ -2,11 +2,8 @@
 
 package no.skatteetaten.aurora.databasehotel.dao
 
-import java.util.Date
 import org.springframework.jdbc.core.RowMapper
 
 var toSchema: RowMapper<Schema> = RowMapper { rs, _ ->
-    Schema(
-        rs.getString("username"),
-        rs.getTimestamp("lastLogin")?.let { Date(it.time) })
+    rs.run { Schema(getString("username"), getDate("lastLogin")) }
 }
