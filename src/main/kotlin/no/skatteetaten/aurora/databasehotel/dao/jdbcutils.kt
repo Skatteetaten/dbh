73d1d15b4880b46fbf7822f5589c63b7fcded5eb
Kim Lindtner <Kim.Lindtner@skatteetaten.no>
2020-04-29 13:59:11 +0200

    Could not convert date toInstant, changed from date back to timestamp

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/dao/jdbcutils.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/dao/jdbcutils.kt
@@ -5,5 +5,5 @@ package no.skatteetaten.aurora.databasehotel.dao
 import org.springframework.jdbc.core.RowMapper
 
 var toSchema: RowMapper<Schema> = RowMapper { rs, _ ->
-    rs.run { Schema(getString("username"), getDate("lastLogin")) }
+    rs.run { Schema(getString("username"), getTimestamp("lastLogin")) }
 }
