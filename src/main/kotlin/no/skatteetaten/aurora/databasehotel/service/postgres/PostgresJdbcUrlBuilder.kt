1c8f30f1a8325ce45832cc5fb96ea5f981f43a78
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2020-08-20 14:33:04 +0200

    fix azure jdbc url

--- src/main/kotlin/no/skatteetaten/aurora/databasehotel/service/postgres/PostgresJdbcUrlBuilder.kt
+++ src/main/kotlin/no/skatteetaten/aurora/databasehotel/service/postgres/PostgresJdbcUrlBuilder.kt
@@ -1,9 +1,19 @@
 package no.skatteetaten.aurora.databasehotel.service.postgres
 
+import mu.KotlinLogging
 import no.skatteetaten.aurora.databasehotel.service.JdbcUrlBuilder
+
+private val logger = KotlinLogging.logger {}
 
 class PostgresJdbcUrlBuilder : JdbcUrlBuilder {
 
-    override fun create(dbHost: String, port: Int, database: String?): String =
-        "jdbc:postgresql://$dbHost:$port/$database"
+    override fun create(dbHost: String, port: Int, database: String?): String {
+        val jdbc = if (dbHost.contains("azure")) {
+            "jdbc:postgresql://$dbHost:$port/$database?ssl=true&sslmode=require"
+        } else {
+            "jdbc:postgresql://$dbHost:$port/$database"
+        }
+        logger.info("Jdbc url is $jdbc")
+        return jdbc
+    }
 }
