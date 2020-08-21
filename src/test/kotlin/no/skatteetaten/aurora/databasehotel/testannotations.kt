16e10d228c480b693f7028e67b0654594a8d673d
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2020-08-21 14:11:33 +0200

    Enable devtools properties during testing

--- src/test/kotlin/no/skatteetaten/aurora/databasehotel/testannotations.kt
+++ src/test/kotlin/no/skatteetaten/aurora/databasehotel/testannotations.kt
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.json.JsonTest
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 @EnabledIfSystemProperty(named = "test.include-oracle-tests", matches = "true")
+@TestPropertySource(locations = ["file:\${HOME}/.spring-boot-devtools.properties"])
 internal annotation class OracleTest
 
 @Target(AnnotationTarget.CLASS)
@@ -17,4 +19,5 @@ internal annotation class OracleTest
 @ExtendWith(SpringExtension::class)
 @JsonTest
 @ContextConfiguration(classes = [OracleConfig::class, PostgresConfig::class, TestDataSources::class, DatabaseInstanceInitializer::class])
+@TestPropertySource(locations = ["file:\${HOME}/.spring-boot-devtools.properties"])
 internal annotation class DatabaseTest
